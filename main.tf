3666c17f9d57212637dfe0553b2e7de89e34d40e
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-17 12:51:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "natural-region-452705-m6"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "vm-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
