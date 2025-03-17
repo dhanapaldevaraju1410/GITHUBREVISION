880a93d04bd4367a25d7437a5b86a2de3486de11
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-17 14:09:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
 provider "google" {
-  project     = "your-project-id"
-  region      = "us-central1"
-  credentials = var.google_credentials
+  project = "natural-region-452705-m6"
+  region  = "us-central1"
 }
 
-variable "google_credentials" {
-  type = string
+resource "google_project_service" "compute" {
+  project = "natural-region-452705-m6"
+  service = "compute.googleapis.com"
 }
 
 resource "google_compute_instance" "default" {
-  name         = "vm-instance"
+  depends_on = [google_project_service.compute]
+
+  name         = "my-vm"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-    }
+    access_config {}
   }
 }
