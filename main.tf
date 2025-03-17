ae71a94d1bdd7d4ae008f2e03061ef7ed69a6cb6
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-17 12:27:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  credentials = file("credentials.json")
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
